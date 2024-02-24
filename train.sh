4032b0844be098bcb344346f8974efd3dbd9acfb
yuhang <67406339@qq.com>
2024-02-24 12:50:57 +0800

    delete --single-cls

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python train.py --data dataset/llvip.yaml --weights '' --hyp configs/hyp.scratch-med.yaml --single-cls --optimizer SGD --batch-size 8 --epochs 300 --img 1280 --name rgbt --device 1
+python train.py --data dataset/llvip.yaml --weights '' --hyp configs/hyp.scratch-low.yaml --optimizer SGD --batch-size 8 --epochs 300 --img 1280 --name rgbt --device 1
